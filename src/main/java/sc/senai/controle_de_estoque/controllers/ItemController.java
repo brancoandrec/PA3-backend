08af9082898d67bca4093bd297757f6c92bbd623
eskivx <repsfit@hotmail.com>
2024-11-24 10:52:18 -0300

    mudando generation type pra identity e finalizando item controller

--- src/main/java/sc/senai/controle_de_estoque/controllers/ItemController.java
+++ src/main/java/sc/senai/controle_de_estoque/controllers/ItemController.java
@@ -3,10 +3,12 @@ package sc.senai.controle_de_estoque.controllers;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import sc.senai.controle_de_estoque.entities.DTO.CriarItemDTO;
 import sc.senai.controle_de_estoque.entities.Item;
 import sc.senai.controle_de_estoque.services.ItemService;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/item")
@@ -29,9 +31,56 @@ public class ItemController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<?> addItem(@RequestBody Item item){
+    public ResponseEntity<?> criarItem(@RequestBody CriarItemDTO item){
         try{
+            Item itemCriado = itemService.criarItem(item);
+            return ResponseEntity.ok(itemCriado);
 
+        } catch (Exception ex) {
+            return new ResponseEntity<>("Erro ao criar item ", HttpStatusCode.valueOf(504));
         }
     }
+
+    @PutMapping("/alterar/{codigo}")
+    public ResponseEntity<?> atualizarItem(@PathVariable Long codigo, @RequestBody Item item) {
+        try {
+
+            Item itemAtualizado = itemService.atualizarItem(codigo, item);
+
+
+            return ResponseEntity.ok(itemAtualizado);
+        } catch (Exception ex) {
+
+            return new ResponseEntity("Erro de Consulta", HttpStatusCode.valueOf(504));
+        }
+    }
+
+
+
+    @DeleteMapping("/excluir/{codigo}")
+    public ResponseEntity<?> excluiritem(@PathVariable Long codigo){
+        try {
+            itemService.excluirItem(codigo);
+            return ResponseEntity.ok("Excluída com Sucesso");
+        }catch(Exception ex) {
+            return new ResponseEntity("Erro de Consulta", HttpStatusCode.valueOf(504));
+        }
+    }
+
+    @GetMapping("/buscar/{codigo}")
+    public ResponseEntity<?> buscaritemPorCodigo(@PathVariable Long codigo){
+        try {
+            Optional<Item> item = itemService.BuscarPorId(codigo);
+            if(Optional.ofNullable(item).isPresent())
+                return ResponseEntity.ok(item.get());
+            else
+                return ResponseEntity.notFound().build();
+
+        }catch(Exception ex) {
+            return new ResponseEntity("Erro de Consulta", HttpStatusCode.valueOf(504));
+        }
+    }
+
+
+
 }
