d3a46919cb845aafb961c3d753528369b5f2ec32
eskivx <repsfit@hotmail.com>
2024-11-24 10:38:12 -0300

    usuario service

---
+++ src/main/java/sc/senai/controle_de_estoque/entities/DTO/CriarItemDTO.java
@@ -0,0 +1,40 @@
+package sc.senai.controle_de_estoque.entities.DTO;
+
+public class CriarItemDTO {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private String tipo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
